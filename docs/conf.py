6512797b0e5dc45baec8fdc312a8acd4d75c0d15
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2025-01-04 14:56:04 +0800

    Update autoapi directory path in documentation configuration
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- docs/conf.py
+++ docs/conf.py
@@ -123,4 +123,4 @@ intersphinx_mapping = {
     "deepmd": ("https://docs.deepmodeling.com/projects/deepmd/", None),
     "torch": ("https://pytorch.org/docs/stable/", None),
 }
-autoapi_dirs = ["deepmd_gnn"]
+autoapi_dirs = ["../deepmd_gnn"]
